82c32c329bff45a8bc24c2e30497f2051f0d03ba
saenglert <sascha@englerts.de>
2022-10-06 21:36:19 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,26 +1,47 @@
-import { of, switchMap, concatMap, exhaustMap, fromEvent, interval, take, map } from 'rxjs';
+import {
+  of,
+  switchMap,
+  concatMap,
+  exhaustMap,
+  fromEvent,
+  interval,
+  take,
+  map,
+  mergeAll,
+} from 'rxjs';
 
-/*
- * # Map
- * 
- * Wende eine Funktion auf alle Elemente einer Liste an
- * 
- * ## Switch
- * 
- * Wechsle (switch) zu einem neuen Observable
+/**
+ * # Join
+ *
+ * Verbinde eine Liste von Elementen
  */
 
-// const clicks = fromEvent(document, 'click');
-// const result = clicks.pipe(
-//   switchMap(() => interval(1000))
-// );
-// result.subscribe(x => console.log(x));
+/**
+ * ## Merge All
+ * 
+ * Fasse alle inneren Observables zu einem einzelnen zusammen (merge)
+ */
+const clicks = fromEvent(document, 'click');
+const higherOrder = clicks.pipe(map(() => interval(1000)));
+const firstOrder = higherOrder.pipe(mergeAll());
 
-/*
- * # Concat
+firstOrder.subscribe((x) => console.log(x));
+
+/**
+ * # Map
  *
- * Wende eine Funktion auf alle Elemente eines Source-Observables an,
- * die wiederum Inner-Observables enthalten kann
+ * Wende eine Operation auf alle Elemente einer Liste an
+ *
+ * # Flat Map
+ *
+ * Glätte eine Liste von Listen und wende auf jedes
+ * Elemente eine Operation an
+ */
+
+/**
+ * ## ConcatMap
+ *
+ * Verkette(concat) die Werte alle Observables
  */
 
 // const clicks = fromEvent(document, 'click');
@@ -29,9 +50,26 @@ import { of, switchMap, concatMap, exhaustMap, fromEvent, interval, take, map } 
 // );
 // result.subscribe(x => console.log(x));
 
+/**
+ * ## SwitchMap
+ *
+ * Wechsle (switch) zum nächsten Observable sobald es aktiv wird
+ */
 
-const clicks = fromEvent(document, 'click');
-const result = clicks.pipe(
-  exhaustMap(() => interval(1000).pipe(take(5)))
-);
-result.subscribe(x => console.log(x));
+// const clicks = fromEvent(document, 'click');
+// const result = clicks.pipe(
+//   switchMap(() => interval(1000))
+// );
+// result.subscribe(x => console.log(x));
+
+/**
+ * ## ExhaustMap
+ *
+ * Ignoriere neue Observables, bis das aktuelle voll ausgeschópft (exhausted) ist.
+ */
+
+// const clicks = fromEvent(document, 'click');
+// const result = clicks.pipe(
+//   exhaustMap(() => interval(1000).pipe(take(5)))
+// );
+// result.subscribe(x => console.log(x));
